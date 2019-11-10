bed6b27b7b143dab0295ef7bfe69d77ab6039447
fuat tatar <fuat.tatar@hotmail.com>
2019-11-10 22:20:36 +0300

    System jobs grid view is added

--- AsynchronousSolutionImport/AsyncSolutionImportPluginControl.Designer.cs
+++ AsynchronousSolutionImport/AsyncSolutionImportPluginControl.Designer.cs
@@ -28,6 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.btnSelectSolution = new System.Windows.Forms.Button();
             this.gbSettings = new System.Windows.Forms.GroupBox();
             this.cbPublishAfterImport = new System.Windows.Forms.CheckBox();
@@ -37,8 +39,18 @@
             this.cbSkipProductUpdateDependencies = new System.Windows.Forms.CheckBox();
             this.cbHoldingSolution = new System.Windows.Forms.CheckBox();
             this.cbConvertToManaged = new System.Windows.Forms.CheckBox();
+            this.dgvAsyncImportJobs = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.JobName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CreatedOn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.StartedOn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CompletedOn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.StatusCode = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FriendlyMessage = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Message = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.gbSettings.SuspendLayout();
             this.gbAdvanceSettings.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAsyncImportJobs)).BeginInit();
             this.SuspendLayout();
             // 
             // btnSelectSolution
@@ -131,22 +143,121 @@
             this.cbConvertToManaged.Text = "Convert To Managed";
             this.cbConvertToManaged.UseVisualStyleBackColor = true;
             // 
+            // dgvAsyncImportJobs
+            // 
+            this.dgvAsyncImportJobs.AllowUserToAddRows = false;
+            this.dgvAsyncImportJobs.AllowUserToDeleteRows = false;
+            this.dgvAsyncImportJobs.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAsyncImportJobs.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.JobName,
+            this.CreatedOn,
+            this.StartedOn,
+            this.CompletedOn,
+            this.StatusCode,
+            this.FriendlyMessage,
+            this.Message});
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvAsyncImportJobs.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvAsyncImportJobs.Location = new System.Drawing.Point(267, 116);
+            this.dgvAsyncImportJobs.Name = "dgvAsyncImportJobs";
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 5.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvAsyncImportJobs.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvAsyncImportJobs.Size = new System.Drawing.Size(1003, 331);
+            this.dgvAsyncImportJobs.TabIndex = 7;
+            this.dgvAsyncImportJobs.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DgvAsyncImportJobs_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(263, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "System Jobs";
+            // 
+            // JobName
+            // 
+            this.JobName.Frozen = true;
+            this.JobName.HeaderText = "JobName";
+            this.JobName.Name = "JobName";
+            this.JobName.Width = 120;
+            // 
+            // CreatedOn
+            // 
+            this.CreatedOn.Frozen = true;
+            this.CreatedOn.HeaderText = "CreatedOn";
+            this.CreatedOn.MinimumWidth = 20;
+            this.CreatedOn.Name = "CreatedOn";
+            this.CreatedOn.Width = 120;
+            // 
+            // StartedOn
+            // 
+            this.StartedOn.Frozen = true;
+            this.StartedOn.HeaderText = "Started On";
+            this.StartedOn.MinimumWidth = 20;
+            this.StartedOn.Name = "StartedOn";
+            this.StartedOn.Width = 120;
+            // 
+            // CompletedOn
+            // 
+            this.CompletedOn.Frozen = true;
+            this.CompletedOn.HeaderText = "Completed On";
+            this.CompletedOn.MinimumWidth = 20;
+            this.CompletedOn.Name = "CompletedOn";
+            this.CompletedOn.Width = 120;
+            // 
+            // StatusCode
+            // 
+            this.StatusCode.Frozen = true;
+            this.StatusCode.HeaderText = "Status Code";
+            this.StatusCode.MinimumWidth = 8;
+            this.StatusCode.Name = "StatusCode";
+            // 
+            // FriendlyMessage
+            // 
+            this.FriendlyMessage.Frozen = true;
+            this.FriendlyMessage.HeaderText = "FriendlyMessage";
+            this.FriendlyMessage.Name = "FriendlyMessage";
+            // 
+            // Message
+            // 
+            this.Message.Frozen = true;
+            this.Message.HeaderText = "Message";
+            this.Message.Name = "Message";
+            this.Message.Width = 400;
+            // 
             // AsyncSolutionImportPluginControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvAsyncImportJobs);
             this.Controls.Add(this.gbAdvanceSettings);
             this.Controls.Add(this.btnImport);
             this.Controls.Add(this.txtSolutionPathText);
             this.Controls.Add(this.gbSettings);
             this.Controls.Add(this.btnSelectSolution);
             this.Name = "AsyncSolutionImportPluginControl";
-            this.Size = new System.Drawing.Size(830, 450);
+            this.Size = new System.Drawing.Size(1270, 450);
             this.Load += new System.EventHandler(this.AsyncSolutionImportPluginControl_Load);
             this.gbSettings.ResumeLayout(false);
             this.gbSettings.PerformLayout();
             this.gbAdvanceSettings.ResumeLayout(false);
             this.gbAdvanceSettings.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAsyncImportJobs)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -162,5 +273,14 @@
         private System.Windows.Forms.CheckBox cbConvertToManaged;
         private System.Windows.Forms.CheckBox cbHoldingSolution;
         private System.Windows.Forms.CheckBox cbSkipProductUpdateDependencies;
+        private System.Windows.Forms.DataGridView dgvAsyncImportJobs;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn JobName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CreatedOn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn StartedOn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CompletedOn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn StatusCode;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FriendlyMessage;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Message;
     }
 }
--- AsynchronousSolutionImport/AsyncSolutionImportPluginControl.cs
+++ AsynchronousSolutionImport/AsyncSolutionImportPluginControl.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.Crm.Sdk.Messages;
 using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Metadata;
 using Microsoft.Xrm.Sdk.Query;
 using System;
 using System.IO;
+using System.Linq;
 using System.Windows.Forms;
 using XrmToolBox.Extensibility;
 
@@ -11,6 +13,26 @@ namespace AsynchronousSolutionImport
 {
     public partial class AsyncSolutionImportPluginControl : PluginControlBase
     {
+        private const string DEFAULT_TEXT = "Select the solution you want to import";
+        private const string ASYNCOPERATION = "asyncoperation";
+        private const string MESSAGE_INCCORRECT_PATH = "Incorrect path. Please select solution zip file";
+        private const string MESSAGE_IMPORT_STARTED = "Solution import process is started. Check it's progress in Settings>System Job ";
+        private const string MESSAGE_PLEASE_SELECT_SOLUTION = "Please select the solution zip file";
+        private const string MESSAGE_UPLOADING_SOLUTION = "Uploading solution";
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+
+        enum JobStatus
+        {
+            Succeeded = 30,
+            Failed = 31,
+            Canceled = 32,
+            InProgress = 20,
+            Pausing = 21,
+            Cancelling = 22,
+            WaitingForResources = 0,
+            Waiting = 10
+        }
+
         public AsyncSolutionImportPluginControl()
         {
             InitializeComponent();
@@ -18,7 +40,7 @@ namespace AsynchronousSolutionImport
 
         private void AsyncSolutionImportPluginControl_Load(object sender, EventArgs e)
         {
-           
+            InitTimer();
         }
 
         private void BtnClose_Click(object sender, EventArgs e)
@@ -26,14 +48,12 @@ namespace AsynchronousSolutionImport
             CloseTool();
         }
 
-        OpenFileDialog openFileDialog = new OpenFileDialog();
-
         private void BtnSelectSolution_Click(object sender, EventArgs e)
         {
             openFileDialog.Filter = "Zip Files|*.zip";
-            openFileDialog.Title = "Please select the solution zip file";
+            openFileDialog.Title = MESSAGE_PLEASE_SELECT_SOLUTION;
 
-            if (openFileDialog.ShowDialog()==DialogResult.OK)
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
                 string filePath = openFileDialog.FileName;
                 string fileName = openFileDialog.SafeFileName;
@@ -42,11 +62,37 @@ namespace AsynchronousSolutionImport
             }
         }
 
+        private void BtnImport_Click(object sender, EventArgs e)
+        {
+            var solutionPath = txtSolutionPathText.Text;
+
+            if (string.IsNullOrEmpty(solutionPath) || solutionPath == DEFAULT_TEXT)
+            {
+                MessageBox.Show(MESSAGE_INCCORRECT_PATH);
+
+                return;
+            }
+
+            ExecuteAsyncRequest importRequest = PrepareImportRequest
+                (
+                solutionPath,
+                cbPublishAfterImport.Checked,
+                cbConvertToManaged.Checked,
+                cbHoldingSolution.Checked,
+                cbSkipProductUpdateDependencies.Checked
+
+                );
+
+            RunImportRequest(importRequest);
+
+            InitTimer();
+        }
+
         private void RunImportRequest(ExecuteAsyncRequest request)
         {
             WorkAsync(new WorkAsyncInfo
             {
-                Message = "Uploading solution",
+                Message = MESSAGE_UPLOADING_SOLUTION,
                 Work = (worker, args) =>
                 {
                     args.Result = (ExecuteAsyncResponse)Service.Execute(request);
@@ -63,7 +109,7 @@ namespace AsynchronousSolutionImport
 
                     if (result != null)
                     {
-                        MessageBox.Show("Solution import process is started. Check it's progress in Settings>System Job ");
+                        MessageBox.Show(MESSAGE_IMPORT_STARTED);
                     }
                 }
             });
@@ -71,6 +117,11 @@ namespace AsynchronousSolutionImport
 
         private static ExecuteAsyncRequest PrepareImportRequest(string filePath, params bool[] values)
         {
+
+            //ParameterCollection parameterCollection = new ParameterCollection();
+            //parameterCollection.Add(new System.Collections.Generic.KeyValuePair<string, object>("name", "test"));
+
+
             ExecuteAsyncRequest request = new ExecuteAsyncRequest
             {
                 Request = new ImportSolutionRequest
@@ -81,36 +132,91 @@ namespace AsynchronousSolutionImport
                     ConvertToManaged = values[1],
                     HoldingSolution = values[2],
                     SkipProductUpdateDependencies = values[3]
-                    
-                    
+                   
                 }
             };
 
             return request;
         }
 
-        private void BtnImport_Click(object sender, EventArgs e)
+        private Timer timer1;
+
+        public void InitTimer()
         {
-            var solutionPath = txtSolutionPathText.Text;
+            timer1 = new Timer();
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Interval = 20000; // in miliseconds
+            timer1.Start();
+        }
 
-            if (string.IsNullOrEmpty(solutionPath))
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            dgvAsyncImportJobs.Rows.Clear();
+            EntityCollection systemjobs = RetrieveSystemImportJobs();
+            AddImportJobsToGrid(systemjobs);
+        }
+
+        private void AddImportJobsToGrid(EntityCollection systemjobs)
+        {
+            foreach (var job in systemjobs.Entities)
             {
-                MessageBox.Show("Incorrect path. Please select solution zip file");
+                string jobName = job.Attributes["name"].ToString();
+                var createdOn = Convert.ToString(job.GetAttributeValue<DateTime?>("createdon"));
+                var startedOn = Convert.ToString(job.GetAttributeValue<DateTime?>("startedon"));
+                var completedOn = Convert.ToString(job.GetAttributeValue<DateTime?>("completedon"));
+                var statusCode = Enum.GetName(typeof(JobStatus), ((OptionSetValue)job.Attributes["statuscode"]).Value);
+                string friendlyMessage = job.GetAttributeValue<string>("friendlymessage");
+                string message = job.GetAttributeValue<string>("message");
 
-                return;
+
+                string[] row = {
+                    jobName,
+                    createdOn,
+                    startedOn,
+                    completedOn,
+                    statusCode,
+                    friendlyMessage,
+                    message
+                };
+
+                dgvAsyncImportJobs.Rows.Add(row);
             }
 
-            ExecuteAsyncRequest importRequest = PrepareImportRequest
-                (
-                solutionPath,
-                cbPublishAfterImport.Checked,
-                cbConvertToManaged.Checked,
-                cbHoldingSolution.Checked,
-                cbSkipProductUpdateDependencies.Checked
+            dgvAsyncImportJobs.AutoResizeColumns();
+            dgvAsyncImportJobs.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+        }
 
-                );
+        private EntityCollection RetrieveSystemImportJobs()
+        {
+            QueryExpression systemJobsQueryExpression = new QueryExpression
+            {
+                EntityName = ASYNCOPERATION,
+                ColumnSet = new ColumnSet("name", "createdon", "completedon", "regardingobjectid", "asyncoperationid", "startedon", "messagename", "statuscode", "friendlymessage", "message"),
+                Criteria = new FilterExpression
+                {
+                    Conditions = {
+                        new ConditionExpression
+                        {
+                            AttributeName = "name",
+                            Operator = ConditionOperator.Equal,
+                            Values = { "ImportSolution" }
+                        }
+                     }
+                },
+            };
 
-            RunImportRequest(importRequest);
+            systemJobsQueryExpression.AddOrder("createdon", OrderType.Descending);
+            systemJobsQueryExpression.PageInfo.PageNumber = 1;
+            systemJobsQueryExpression.PageInfo.Count = 10;
+
+            var systemjobs = Service.RetrieveMultiple(systemJobsQueryExpression);
+
+            return systemjobs;
+        }
+
+        private void DgvAsyncImportJobs_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
